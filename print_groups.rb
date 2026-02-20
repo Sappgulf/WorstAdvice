e2fe3ca21e4b8e42d1b759a86091ac96c8a8c48c
Sappgulf <sappgulf@gmail.com>
2026-02-20 02:16:59 -0600

    chore: Remove Ruby testing files

--- print_groups.rb
+++
@@ -1,6 +0,0 @@
-require 'xcodeproj'
-project = Xcodeproj::Project.open('/Users/austinbeatty/Downloads/WorstAdvice/Badvice.xcodeproj')
-puts "Main group children:"
-project.main_group.children.each do |c|
-  puts " - #{c.name} (#{c.path})"
-end
--- test.rb
+++
@@ -1,5 +0,0 @@
-require 'xcodeproj'
-project = Xcodeproj::Project.open('Badvice.xcodeproj')
-project.main_group.children.each do |c|
-  puts "#{c.name || c.path}"
-end
